d1510dfd3aac741ec7b35bd4dc00600717aececb
Douglas Petla <douglas.petla@rangle.io>
2021-01-22 14:37:01 -0500

    Colors

---
+++ styles/colors.ts
@@ -0,0 +1,7 @@
+export const black = '#000000';
+export const charcoal = '#111111';
+export const slateGray = '#757575';
+export const mediumGray = '#D3D3D3';
+export const lightGray = '#E8E8E9';
+export const offWhite = '#F6F6F6';
+export const white = '#FFFFFF';
