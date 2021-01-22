9f3763b700c78b4a536732bcb467fdfa928d7dc0
Douglas Petla <douglas.petla@rangle.io>
2021-01-22 17:04:13 -0500

    VideoPlayer useReducer

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,5 +1,5 @@
 import {useDeviceOrientation} from '@react-native-community/hooks';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useReducer} from 'react';
 import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
 import YouTube from 'react-native-youtube';
 import {red} from '../styles/colors';
@@ -15,17 +15,43 @@ interface Props {
   showFullscreenButton?: boolean;
 }
 
+interface State {
+  status: string;
+  isFullscreen: boolean;
+}
+
+const initialState: State = {
+  status: 'paused',
+  isFullscreen: false,
+};
+
+function reducer(state: State, action): State {
+  switch (action.type) {
+    case 'STATUS_CHANGE':
+      return {...state, status: action.status};
+    case 'ORIENTATION_LANDSCAPE':
+      return {
+        ...state,
+        isFullscreen: state.status === 'playing' && action.landscape,
+      };
+    case 'FULLSCREEN_CHANGE':
+      return {...state, isFullscreen: action.isFullscreen};
+    default:
+      throw new Error(`Error: the action ${action.type} is not supported.`);
+  }
+}
+
 const VideoPlayer = ({
   item,
   autoplay = false,
   autoFullscreen = false,
   showFullscreenButton = true,
 }: Props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const {data: videos, error, run} = useAsync<Video[]>([]);
-  const [status, setStatus] = useState('paused');
-  const [fullscreen, setFullscreen] = useState(autoFullscreen);
   const orientation = useDeviceOrientation();
 
+  const {status, isFullscreen} = state;
   const video = videos.filter((video) => video.type === 'Trailer')[0];
 
   useEffect(() => {
@@ -36,7 +62,10 @@ const VideoPlayer = ({
 
   useEffect(() => {
     if (!autoFullscreen) {
-      setFullscreen(status === 'playing' && orientation.landscape);
+      dispatch({
+        type: 'ORIENTATION_LANDSCAPE',
+        isLandscape: orientation.landscape,
+      });
     }
   }, [autoFullscreen, status, orientation]);
 
@@ -44,21 +73,23 @@ const VideoPlayer = ({
     <YouTube
       videoId={video.key}
       play={status === 'playing' || autoplay}
-      fullscreen={fullscreen}
+      fullscreen={isFullscreen}
       showFullscreenButton={showFullscreenButton}
       controls={2}
       modestbranding={true}
       showinfo={false}
       rel={false}
       style={styles.player}
-      onChangeState={(e) => setStatus(e.state)}
-      onChangeFullscreen={(e) => setFullscreen(e.isFullscreen)}
+      onChangeState={(e) => dispatch({type: 'STATUS_CHANGE', status: e.state})}
+      onChangeFullscreen={(e) =>
+        dispatch({type: 'FULLSCREEN_CHANGE', isFullscreen: e.isFullscreen})
+      }
     />
   ) : error ? (
     <Text>Error: {error.message}</Text>
   ) : (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <ActivityIndicator size={fullscreen ? 'large' : 'small'} color={red} />
+      <ActivityIndicator size={isFullscreen ? 'large' : 'small'} color={red} />
     </View>
   );
 };
